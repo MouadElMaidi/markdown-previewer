c8756b9243abe0426cbb9bbe11110d437c53f467
MouadElMaidi <mouadlmaidi@gmail.com>
2022-07-11 20:09:41 +0100

    final additions to project

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import {defaultText} from "./default-text"
 
 function App() {
   const [editorText, setEditorText] = React.useState(defaultText);
-  //console.log(marked.parse('# Marked in the browser\n\nRendered by **marked**.'));
+  
   function handleClick(event) {
     setEditorText(event.target.value);
   }
-console.log(defaultText)
+
   return (
     <div className="app">
       <Editor handleClick={handleClick} value={editorText} />
